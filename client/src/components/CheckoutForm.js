2f7b63823bddfb06475d611e6b4b8da61b598b0d
unknown <tompsherman@gmail.com>
2020-10-02 13:24:10 -0400

    questions answered

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -12,41 +12,59 @@ const CheckoutForm = (props) => {
     <>
       <form onSubmit={handleSubmit}>
         <h2>Checkout Form</h2>
-        <label>
+        <label htmlFor="firstName">
           First Name:
           <input
+            id="firstName"
             name="firstName"
             value={values.firstName}
             onChange={handleChanges}
           />
         </label>
-        <label>
+        <label htmlFor="lastName">
           Last Name:
           <input
+            id="lastName"
             name="lastName"
             value={values.lastName}
             onChange={handleChanges}
           />
         </label>
-        <label>
+        <label htmlFor="address">
           Address:
           <input
+            id="address"
             name="address"
             value={values.address}
             onChange={handleChanges}
           />
         </label>
-        <label>
+        <label htmlFor="city">
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input
+            id="city"
+            name="city"
+            value={values.city}
+            onChange={handleChanges}
+          />
         </label>
-        <label>
+        <label htmlFor="state">
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input
+            id="state"
+            name="state"
+            value={values.state}
+            onChange={handleChanges}
+          />
         </label>
-        <label>
+        <label htmlFor="zip">
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input
+            id="zip"
+            name="zip"
+            value={values.zip}
+            onChange={handleChanges}
+          />
         </label>
         <button>Checkout</button>
       </form>
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,41 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import App from "../App";
 
 // Write up the two tests here and make sure they are testing what the title shows
+describe("test checkout form", () => {
+  test("app renders without error", () => {
+    render(<CheckoutForm />);
+  });
 
-test("form header renders", () => {});
+  test("form header renders", async () => {
+    render(<App />);
 
-test("form shows success message on submit with form details", () => {});
+    const headerRender = screen.getByText(/react plants/i);
+  });
+
+  test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm />);
+
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const lastNameInput = screen.getByLabelText(/last name/i);
+    const addressInput = screen.getByLabelText(/address/i);
+    const cityInput = screen.getByLabelText(/city/i);
+    const stateInput = screen.getByLabelText(/state/i);
+    const zipInput = screen.getByLabelText(/zip/i);
+
+    const submitButton = screen.getByRole("button", { name: /checkout/i });
+
+    fireEvent.change(firstNameInput, { target: { value: "tom" } });
+    fireEvent.change(lastNameInput, { target: { value: "tom" } });
+    fireEvent.change(addressInput, { target: { value: "tom" } });
+    fireEvent.change(cityInput, { target: { value: "tom" } });
+    fireEvent.change(stateInput, { target: { value: "tom" } });
+    fireEvent.change(zipInput, { target: { value: "tom" } });
+
+    fireEvent.click(submitButton);
+
+    await screen.findByText(/woo-hoo/i);
+  });
+});
